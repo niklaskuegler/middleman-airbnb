e5a8a9a6a9c92e23e736d7e4cd9c51518cb3c3b2
Niklas Kuegler <niklas.kuegler@gmail.com>
2018-05-28 17:19:51 +0200

    Added CSS for colors and fonts to over-ride Bootstrap theme

--- config.rb
+++ config.rb
@@ -20,3 +20,7 @@ activate :deploy do |deploy|
   deploy.build_before = true
   deploy.deploy_method = :git
 end
+
+["papillard", "ssaunier", "monsieurpaillard", "cpeh", "werner-sven", "willyder1te", "niklaskuegler"].each do |name|
+  proxy "/flats/#{name}.html", "/flats/show.html", locals: { owner: name }, ignore: true
+end
